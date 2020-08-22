45c5e99372ad35a2f867a4009f36aeef348bf032
squti <sqquti@gmail.com>
2020-08-22 15:52:14 +0430

    Initial commit

---
+++ app/src/main/java/com/github/squti/exoplayermkvsample/MainActivity.java
@@ -0,0 +1,52 @@
+package com.github.squti.exoplayermkvsample;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PlayerView playerView;
+    private SimpleExoPlayer player;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        playerView = findViewById(R.id.player_view);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        player = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
+        playerView.setPlayer(player);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(
+                this,
+                Util.getUserAgent(this, getString(R.string.app_name)));
+        ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse("file:///android_asset/jellyfish-3-mbps-hd-h264.mkv"));
+
+        player.prepare(mediaSource);
+        player.setPlayWhenReady(true);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        playerView.setPlayer(null);
+        player.release();
+    }
+}
